aebf4097e2ddece0d0f6f8794ce96746aa14e133
Iuliia Ivashko <yulia.ivashko@gmail.com>
2026-04-03 16:20:53 +0300

    register ngrok tunnel provider in server registry

--- packages/web/server/index.js
+++ packages/web/server/index.js
@@ -12,6 +12,7 @@ import { createTunnelAuth } from './lib/opencode/tunnel-auth.js';
 import { createManagedTunnelConfigRuntime } from './lib/tunnels/managed-config.js';
 import { createTunnelProviderRegistry } from './lib/tunnels/registry.js';
 import { createCloudflareTunnelProvider } from './lib/tunnels/providers/cloudflare.js';
+import { createNgrokTunnelProvider } from './lib/tunnels/providers/ngrok.js';
 import { createRequestSecurityRuntime } from './lib/security/request-security.js';
 import {
   TUNNEL_MODE_MANAGED_LOCAL,
@@ -329,6 +330,7 @@ let uiAuthController = null;
 let activeTunnelController = null;
 const tunnelProviderRegistry = createTunnelProviderRegistry([
   createCloudflareTunnelProvider(),
+  createNgrokTunnelProvider(),
 ]);
 tunnelProviderRegistry.seal();
 const tunnelAuthController = createTunnelAuth();
